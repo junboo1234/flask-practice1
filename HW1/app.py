461a71efea6722dbe852b96f286fe121b43f2441
junboo1234 <168405856+junboo1234@users.noreply.github.com>
2026-09-22 12:46:42 +0900

    add templates greet, name, profile
    각 템플릿 링크 추가

--- HW1/app.py
+++ HW1/app.py
@@ -1,20 +1,22 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route("/profile")
 def profile_template_render():
     hobbies = ["운동", "게임", "코딩"]
-
     return render_template("profile.html", habit=hobbies)
+
 
 @app.route("/")
 def name_template_render():
     return render_template("name.html")
 
-@app.route("/greet/<name>")
-def greet_template_render(greet):
-    return render_template("greet.html", greet=greet)
 
+@app.route("/greet/<name>")
+def greet_template_render(name):
+    return render_template("greet.html", name=name)
 
 
 if __name__ == "__main__":
